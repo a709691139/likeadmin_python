2ddd199270a56d93160c6b17a38017ec2654328b
zhengjinpeng-PD <709691139@qq.com>
2025-09-17 17:27:04 +0800

    fix: sh

--- server/run_admin.sh
+++ server/run_admin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # 运行服务
-python3 -m uvicorn asgi:app --port 8000 --reload
+python3 -m uvicorn asgi:app --port 8000 --reload --host 0.0.0.0
--- server/run_front.sh
+++ server/run_front.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # 运行服务
-python3 -m uvicorn asgi_front:app --port 8002 --reload
+python3 -m uvicorn asgi_front:app --port 8002 --reload --host 0.0.0.0
